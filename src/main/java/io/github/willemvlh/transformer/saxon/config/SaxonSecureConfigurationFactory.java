f70605881973e01ea31246bc6e0ece84a875a6eb
Sylvain Balbous <sbalbous@pomlo.fr>
2022-01-12 15:50:39 +0100

    Update Log4j dependency & remove dependency on built-in xerces

--- src/main/java/io/github/willemvlh/transformer/saxon/config/SaxonSecureConfigurationFactory.java
+++ src/main/java/io/github/willemvlh/transformer/saxon/config/SaxonSecureConfigurationFactory.java
@@ -1,31 +1,39 @@
 package io.github.willemvlh.transformer.saxon.config;
 
-import com.sun.org.apache.xerces.internal.parsers.SAXParser;
-import net.sf.saxon.Configuration;
-import net.sf.saxon.lib.Feature;
+import java.io.InputStream;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+
 import org.xml.sax.InputSource;
 
-import javax.xml.transform.sax.SAXSource;
-import java.io.InputStream;
+import net.sf.saxon.Configuration;
+import net.sf.saxon.lib.Feature;
 
 public class SaxonSecureConfigurationFactory extends SaxonConfigurationFactory {
 
-    @Override
-    public Configuration newConfiguration() {
-        Configuration config = new Configuration();
-        config.setAllowedUriTest(uri -> false);
-        config.setConfigurationProperty(Feature.ALLOW_EXTERNAL_FUNCTIONS, false);
-        return config;
-    }
+	@Override
+	public Configuration newConfiguration() {
+		Configuration config = new Configuration();
+		config.setAllowedUriTest(uri -> false);
+		config.setConfigurationProperty(Feature.ALLOW_EXTERNAL_FUNCTIONS, false);
+		return config;
+	}
 
-    @Override
-    public SAXSource newSAXSource(InputStream stream) {
-        SAXParser p = new SAXParser();
-        try {
-            p.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new SAXSource(p, new InputSource(stream));
-    }
+	@Override
+	public SAXSource newSAXSource(InputStream stream) {
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		spf.setNamespaceAware(true);
+
+		try {
+			spf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			SAXParser saxParser = spf.newSAXParser();
+			
+			return new SAXSource(saxParser.getXMLReader(), new InputSource(stream));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
 }
