f54ddd227240525efe8d53c47e6cd361141be7ba
Sylvain Balbous <sbalbous@pomlo.fr>
2022-01-13 11:12:59 +0100

    XSL absolute path management

--- src/main/java/io/github/willemvlh/transformer/app/xslloader/FileLoaderService.java
+++ src/main/java/io/github/willemvlh/transformer/app/xslloader/FileLoaderService.java
@@ -1,22 +1,45 @@
 package io.github.willemvlh.transformer.app.xslloader;
 
-import io.github.willemvlh.transformer.app.ServerOptions;
+import java.io.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
+import io.github.willemvlh.transformer.app.ServerOptions;
 
 @Service
 public class FileLoaderService {
 
-    private final ServerOptions options;
+	private final ServerOptions options;
 
-    @Autowired
-    public FileLoaderService(ServerOptions options) {
-        this.options = options;
-    }
+	@Autowired
+	public FileLoaderService(ServerOptions options) {
+		this.options = options;
+	}
 
-    File getXslFile(String xslServerPath) {
-        return new File(this.options.getXslRootPath(), xslServerPath);
-    }
+	File getXslFile(String xslServerPath) {
+		// If xslServerPath starts with /, it is supposed to be an absolute path.
+		if (xslServerPath.startsWith("/")) {
+			xslServerPath = convertToRelativePath(xslServerPath);
+		}
+
+		return new File(this.options.getXslRootPath(), xslServerPath);
+	}
+
+	/**
+	 * Check that the given absolute path contains the configured xslRootPath and
+	 * convert it to a relative path.
+	 * 
+	 * @param xslAbsolutePath the supposed xsl file absolute path.
+	 * @return the relative path.
+	 * @throws @SecurityException it the absolute path does not includes the
+	 *                            configured xslRootPath.
+	 */
+	String convertToRelativePath(final String xslAbsolutePath) {
+		if (xslAbsolutePath.startsWith(this.options.getXslRootPath())) {
+			return xslAbsolutePath.replace(this.options.getXslRootPath(), "");
+		} else {
+			throw new SecurityException("Using absolute path outside of configured xslRootPath is forbidden.");
+		}
+	}
 }
--- src/test/java/io/github/willemvlh/transformer/app/xslloader/FileLoaderServiceTest.java
+++ src/test/java/io/github/willemvlh/transformer/app/xslloader/FileLoaderServiceTest.java
@@ -2,6 +2,7 @@ package io.github.willemvlh.transformer.app.xslloader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
 
 import io.github.willemvlh.transformer.app.ServerOptions;
@@ -18,5 +19,28 @@ class FileLoaderServiceTest {
 		assertEquals("/xsl/root/path/subpath/stylesheet.xsl", 
 				fileLoaderService.getXslFile("subpath/stylesheet.xsl").getAbsolutePath());
 	}
+	
+	@Test
+	void testAbsolutePath() {
+		ServerOptions options = new ServerOptions();
+		options.setXslRootPath("/xsl/root/path");
+		
+		FileLoaderService fileLoaderService = new FileLoaderService(options);
+		
+		assertEquals("/xsl/root/path/subpath/stylesheet.xsl", 
+				fileLoaderService.getXslFile("/xsl/root/path/subpath/stylesheet.xsl").getAbsolutePath());
+	}
 
+	@Test
+	void testForbiddenAbsolutePath() {
+		ServerOptions options = new ServerOptions();
+		options.setXslRootPath("/xsl/root/path");
+		
+		FileLoaderService fileLoaderService = new FileLoaderService(options);
+		
+		assertThrows(SecurityException.class, () -> {
+			fileLoaderService.getXslFile("/hacked/root/path/subpath/stylesheet.xsl");
+		});
+
+	}
 }
